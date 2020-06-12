11b197be9f378ec546ed733103420ea2965be6f5
John Wallace <John.Wallace@noaa.gov>
2020-06-11 17:37:38 -0700

    /master/R/

--- R/S.R
+++ R/S.R
@@ -3,7 +3,7 @@ S <- function(File, show = TRUE, ...) {
     if (!(is.character(substitute(File))))
         File <- deparse(substitute(File))
 
-     gitAFile(paste0(repoPath, File, ".R"), show = show, ...)     
+     gitAFile(paste0(repoPath, "/master/R/", File, ".R"), show = show, ...)     
 }
 
 
