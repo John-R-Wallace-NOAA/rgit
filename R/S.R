c1f32ed41705624cb418a2de563c0ff3c84e6054
John Wallace <John.Wallace@noaa.gov>
2020-06-11 17:30:01 -0700

    Update S.R

--- R/S.R
+++ R/S.R
@@ -3,7 +3,7 @@ S <- function(File, show = TRUE, ...) {
     if (!(is.character(substitute(File))))
         File <- deparse(substitute(File))
 
-     gitAFile(paste0("John-R-Wallace-NOAA/JRWToolBox/master/R/", File, ".R"), show = show, ...)     
+     gitAFile(paste0(repoPath, File, ".R"), show = show, ...)     
 }
 
 
