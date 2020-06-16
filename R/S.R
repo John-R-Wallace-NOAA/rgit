ff5eb45bf03870cc96fe159e6e73c43f512ecf78
John Wallace <John.Wallace@noaa.gov>
2020-06-15 19:08:51 -0700

    Update S.R

--- R/S.R
+++ R/S.R
@@ -1,11 +1,7 @@
 
 S <- function(File, repoPath. = repoPath, show = TRUE, ...) {
-    assign('.repoPath', repoPath., pos = 1) # Need this so gitAFile sees the external repoPath 
     if (!(is.character(substitute(File))))
         File <- deparse(substitute(File))
-
-     rgit::gitAFile(paste0(.repoPath, "/master/R/", File, ".R"), show = show, ...)     
+    rgit::gitAFile(paste0(repoPath., "/master/R/", File, ".R"), show = show, ...)     
 }
-
-
 
