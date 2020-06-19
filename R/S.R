49876f288acfc41f74259c61b6630664ef533141
John Wallace <John.Wallace@noaa.gov>
2020-06-19 00:47:49 -0700

    subDir arg

--- R/S.R
+++ R/S.R
@@ -1,7 +1,8 @@
 
-S <- function(File, repoPath. = repoPath, gitPath = paste0(repoPath., "/master/R/"), show = TRUE, ...) {
+S <- function(File, repoPath. = repoPath, gitPath = paste0(repoPath., "/master/", subDir, "/"), subDir = 'R', show = TRUE, ...) {
     if (!(is.character(substitute(File))))
         File <- paste0(deparse(substitute(File)), ".R")
     rgit::gitAFile(paste0(gitPath, File), show = show, ...)     
 }
+
 
