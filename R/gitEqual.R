8bb5fbf98bc3dabf05d22a54ccab47e34342c7e0
John Wallace <John.Wallace@noaa.gov>
2020-06-19 02:19:36 -0700

    Create gitEqual.R

---
+++ R/gitEqual.R
@@ -0,0 +1,17 @@
+
+gitEqual <- function(Func, verbose = FALSE, ...) {
+    
+  Func.Name <- deparse(substitute(Func))
+	if(verbose) cat("\n\n", Func.Name, "\n")
+	
+	JRWToolBox::Source(paste0(Func.Name, '.R'))
+	if(verbose) print(eval(parse(text = Func.Name)))
+	
+	argList <- c(list(paste0(Func.Name, '.R'), show = FALSE, viewOnly = TRUE, verbose = verbose), ...)
+	Remote <- do.call(S, argList)
+	if(verbose) print(Remote); flush.console()
+	
+	all.equal(eval(parse(text = Func.Name)), Remote)
+}	
+	
+	
