8533287d9a0700583711ebc909dce74f21115bf2
John Wallace <John.Wallace@noaa.gov>
2020-06-19 16:34:06 -0700

    Bug Fix

--- R/S.R
+++ R/S.R
@@ -1,5 +1,7 @@
 
-S <- function(File, repoPath. = repoPath, gitPath = paste0(repoPath., "/master/", subDir, "/"), subDir = 'R', show = TRUE, ...) {
+
+S <- function(File, repoPath. = repoPath, gitPath = ifelse(is.null(subDir), paste0(repoPath., "/master/"),
+                paste0(repoPath., "/master/", subDir, "/")), subDir = 'R', show = TRUE, ...) {
     if (!(is.character(substitute(File))))
         File <- paste0(deparse(substitute(File)), ".R")
     rgit::gitAFile(paste0(gitPath, File), show = show, ...)     
