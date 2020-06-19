2f5442de7219fcaaee0882ae522922ba8f51cb6f
John Wallace <John.Wallace@noaa.gov>
2020-06-19 15:50:40 -0700

    Bug fix

--- R/gitEqual.R
+++ R/gitEqual.R
@@ -1,8 +1,8 @@
 
 
 gitEqual <- function(..., list = character(), Func, verbose = FALSE, subDir = 'R') {
-    ' # Remote '
-	dots <- match.call(expand.dots = FALSE)$...
+    
+    dots <- match.call(expand.dots = FALSE)$...
     if (length(dots) && !all(vapply(dots, function(x) is.symbol(x) || 
         is.character(x), NA, USE.NAMES = FALSE))) 
         stop("... must contain names or character strings")
@@ -10,24 +10,28 @@ gitEqual <- function(..., list = character(), Func, verbose = FALSE, subDir = 'R
     if (length(names) == 0L) 
         names <- character()
     Func.Name <- .Primitive("c")(list, names)
-	
-	if(verbose) cat("\n\n", Func.Name, "\n")
-	
-	JRWToolBox::Source(paste0(Func.Name, '.R'))
-	assign(Func.Name, eval(parse(text = Func.Name)), pos = 1)
-	if(verbose) {
-	
-	  cat("\nR object in Global area:", ls(pattern = Func.Name), "\n\n")
-	   print(eval(parse(text = Func.Name)))
-	   
-	}
-	
-	argList <- list(paste0(Func.Name, '.R'), show = FALSE, viewOnly = TRUE, verbose = verbose, subDir = subDir)
-	Remote <- do.call(S, argList)
-	if(verbose) print(Remote); flush.console()
-	
-	all.equal(eval(parse(text = Func.Name)), Remote)
-}	
-	
+    
+    if(verbose) {
+       cat("\n\nFunction Name:", Func.Name, "\n\n"); flush.console()
+    }
+    
+    source(paste0(Func.Name, '.R'), local = TRUE)
+    
+    Local <- get(Func.Name)
+    if(verbose) {
+      cat("\n\nLocal file in ", getwd(), ":\n\n", sep = "")
+      print(Local); flush.console()
+    }
+    
+    argList <- list(paste0(Func.Name, '.R'), show = FALSE, viewOnly = TRUE, verbose = verbose, subDir = subDir)
+    Remote <- do.call(S, argList)
+    if(verbose) {
+       cat("\n\nRemote on", repoPath, ":\n\n", sep = "")
+       print(Remote); flush.console()
+    }
+    
+    all.equal(Local, Remote)
+}    
+    
 
 
