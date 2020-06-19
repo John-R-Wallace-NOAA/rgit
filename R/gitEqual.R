dfa09d8c5e0e3413db52aa29f829614f925ee0b1
John Wallace <John.Wallace@noaa.gov>
2020-06-19 03:51:28 -0700

    Update gitEqual.R

--- R/gitEqual.R
+++ R/gitEqual.R
@@ -1,19 +1,25 @@
 
-gitEqual <- function(Func.Name, verbose = FALSE, ...) {
-      
-	if (!(is.character(substitute(Func.Name))))
-           Func.Name <- deparse(substitute(Func.Name))
+gitEqual <- function(..., list = character(), Func, verbose = FALSE, subDir = 'R') {
+    
+	dots <- match.call(expand.dots = FALSE)$...
+    if (length(dots) && !all(vapply(dots, function(x) is.symbol(x) || 
+        is.character(x), NA, USE.NAMES = FALSE))) 
+        stop("... must contain names or character strings")
+    names <- vapply(dots, as.character, "")
+    if (length(names) == 0L) 
+        names <- character()
+    Func.Name <- .Primitive("c")(list, names)
+	
 	if(verbose) cat("\n\n", Func.Name, "\n")
 	
 	JRWToolBox::Source(paste0(Func.Name, '.R'))
 	assign(Func.Name, eval(parse(text = Func.Name)), pos = 1)
 	if(verbose) print(eval(parse(text = Func.Name)))
 	
-	argList <- c(list(paste0(Func.Name, '.R'), show = FALSE, viewOnly = TRUE, verbose = verbose), ...)
+	argList <- list(paste0(Func.Name, '.R'), show = FALSE, viewOnly = TRUE, verbose = verbose, subDir = subDir)
 	Remote <- do.call(S, argList)
 	if(verbose) print(Remote); flush.console()
 	
 	all.equal(eval(parse(text = Func.Name)), Remote)
 }	
-	
 	
