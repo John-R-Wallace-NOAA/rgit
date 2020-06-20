0bac8c897eff9d4f2f591225e8b95e799960ef4b
John Wallace <John.Wallace@noaa.gov>
2020-06-19 19:12:02 -0700

    Update gitEdit.R

--- R/gitEdit.R
+++ R/gitEdit.R
@@ -1,10 +1,10 @@
 gitEdit <- function (File, repoPath. = repoPath, gitPath = ifelse(is.null(subDir), paste0(repoPath., "/master/"),
-                paste0(repoPath., "/master/", subDir, "/")), subDir = 'R')
+                paste0(repoPath., "/master/", subDir, "/")), subDir = 'R', ...)
 {
    if (!(is.character(substitute(File)))) 
         File <- paste0(deparse(substitute(File)), ".R")
     
-    rgit::gitAFile(paste0(gitPath, File), "script", File = File, show = FALSE)
+    rgit::gitAFile(paste0(gitPath, File), "script", File = File, show = FALSE, ...)
     
     if(file.exists('W:/Win_apps/npp/notepad++.exe'))
        system(paste0('C:/Windows/System32/cmd.exe /C start W:/Win_apps/npp/notepad++.exe "', getwd(), '/', File, '"'))
