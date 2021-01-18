4a2b9bf2272ca8582258c66202c8f052721576c8
John Wallace <John.Wallace@noaa.gov>
2021-01-18 02:05:56 -0800

    Switched to xml2::download_html
    gitURL() broken with SSl issues

--- R/sourceFunctionURL.R
+++ R/sourceFunctionURL.R
@@ -1,9 +1,10 @@
 
- sourceFunctionURL <- function(URL) {
-       ' # For more functionality, see gitAFile() in the rgit package ( https://github.com/John-R-Wallace-NOAA/rgit ) which includes gitPush() and git() '
-       require(RCurl)
-       File.ASCII <- tempfile()
-       on.exit(file.remove(File.ASCII))
-       writeLines(paste(readLines(textConnection(RCurl::getURL(URL))), collapse = "\n"), File.ASCII)
-       source(File.ASCII, local = parent.env(environment()))
-    }
+
+sourceFunctionURL <- function (URL) {
+    " # For more functionality, see gitAFile() in the rgit package ( https://github.com/John-R-Wallace-NOAA/rgit ) which includes gitPush() and git() "
+    require(xml2)
+    File.ASCII <- tempfile()
+    on.exit(file.remove(File.ASCII))
+    writeLines(paste0('source("', readLines(textConnection(xml2::download_html(URL))), '")'), File.ASCII)
+    source(File.ASCII, local = parent.env(environment()))
+}
