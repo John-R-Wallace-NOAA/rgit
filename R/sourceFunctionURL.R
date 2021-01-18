a82ed50398693f00ecfb24be24b448fc81a3d82b
John Wallace <John.Wallace@noaa.gov>
2021-01-18 15:09:11 -0800

    Employs use of temp directory

--- R/sourceFunctionURL.R
+++ R/sourceFunctionURL.R
@@ -1,10 +1,13 @@
 
-
 sourceFunctionURL <- function (URL) {
     " # For more functionality, see gitAFile() in the rgit package ( https://github.com/John-R-Wallace-NOAA/rgit ) which includes gitPush() and git() "
     require(xml2)
     File.ASCII <- tempfile()
     on.exit(file.remove(File.ASCII))
+    tempDir <- tempfile()
+    dir.create(tempDir); setwd(tempDir)
+    on.exit(setwd('..'), add = TRUE)
+    on.exit(system(paste0("rm -r -f ", tempDir)), add = TRUE)
     writeLines(paste0('source("', readLines(textConnection(xml2::download_html(URL))), '")'), File.ASCII)
     source(File.ASCII, local = parent.env(environment()))
 }
