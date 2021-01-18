87276ddaec7252ae1d6748f396937b2371b102f7
John Wallace <John.Wallace@noaa.gov>
2021-01-18 02:08:38 -0800

    Older function using getURL()

---
+++ R/sourceFunction_getURL.R
@@ -0,0 +1,10 @@
+
+ sourceFunction_getURL <-function (URL) {
+    " # For more functionality, see gitAFile() in the rgit package ( https://github.com/John-R-Wallace-NOAA/rgit ) which includes gitPush() and git() "
+    require(RCurl)
+    File.ASCII <- tempfile()
+    on.exit(file.remove(File.ASCII))
+    writeLines(paste(readLines(textConnection(RCurl::getURL(URL))), collapse = "\n"), File.ASCII)
+    source(File.ASCII, local = parent.env(environment()))
+}
+
