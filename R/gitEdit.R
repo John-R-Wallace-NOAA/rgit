5343a362a3e835cbf6f57a4b70b09f7f3f786fe6
John Wallace <John.Wallace@noaa.gov>
2021-01-19 03:43:27 -0800

    deleteFileObj = FALSE

--- R/gitEdit.R
+++ R/gitEdit.R
@@ -1,12 +1,11 @@
-gitEdit <- function (File, gitPath = ifelse(is.null(subDir), paste0(repoPath., 
-    "/", branch, "/"), paste0(repoPath., "/", branch, "/", subDir, 
-    "/")), branch = "master", repoPath. = repoPath, subDir = "R", 
-    ...) 
-{
+gitEdit <- function (File, gitPath = ifelse(is.null(subDir), paste0(repoPath., "/", branch, "/"), paste0(repoPath., "/",
+                       branch, "/", subDir,  "/")), branch = "master", repoPath. = repoPath, subDir = "R", ...) {
+    
     if (!(is.character(substitute(File)))) 
         File <- paste0(deparse(substitute(File)), ".R")
-    # rgit::gitAFile(paste0(gitPath, File), "script", File = File, show = FALSE, ...)
-    rgit::gitAFile(paste0(gitPath, File), File = File, show = FALSE, ...)
+    
+    rgit::gitAFile(paste0(gitPath, File), deleteFileObj = FALSE, show = FALSE, ...)
+    
     if (file.exists("W:/Win_apps/npp/notepad++.exe")) 
         system(paste0("C:/Windows/System32/cmd.exe /C start W:/Win_apps/npp/notepad++.exe \"", 
             getwd(), "/", File, "\""))
