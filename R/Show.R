b8ace5a7cd1ad4677e2671d8ad56667812d8de9d
John Wallace <John.Wallace@noaa.gov>
2021-01-19 01:20:44 -0800

    fileName => s.name

--- R/Show.R
+++ R/Show.R
@@ -1,21 +1,18 @@
-
-
 Show <- function(File, gitPath = ifelse(is.null(subDir), paste0(repoPath., "/", branch, "/"), 
          paste0(repoPath., "/", branch, "/", subDir, "/")), branch = 'master', repoPath. = repoPath, subDir = 'R', show = TRUE, ...) {
                 
     if (!(is.character(substitute(File))))
         File <- paste0(deparse(substitute(File)), ".R")
-		
+        
     rgit::sourceFunctionURL(paste0('https://raw.githubusercontent.com/', gitPath, File))
-	
+    
     if(show) {
-	   fileName <- JRWToolBox::get.subs(File, '.')
-       fileName <- fileName[-length(fileName)]
-       fileName <- paste(fileName, collapse = '.')
-	   cat("\n")
-       print(eval(parse(text = fileName), envir = globalenv()))
+       s.name <- JRWToolBox::get.subs(File, '.')
+       s.name <- s.name[-length(s.name)]
+       s.name <- paste(s.name, collapse = '.')
+       cat("\n")
+       print(eval(parse(text = s.name), envir = globalenv()))
     } else
        invisible()
 }
-
 
