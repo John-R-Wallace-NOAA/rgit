9d7c4b462704c70bb9597653530cee9a043a62dc
John Wallace <John.Wallace@noaa.gov>
2023-04-18 19:40:20 -0700

    if httr package is missing then install it

--- R/sourceFunctionURL.R
+++ R/sourceFunctionURL.R
@@ -2,7 +2,7 @@
 sourceFunctionURL <- function (URL) 
 {
     " # For more functionality, see gitAFile() in the rgit package ( https://github.com/John-R-Wallace-NOAA/rgit ) which includes gitPush() and git() "
-    require(httr)
+    if (!any(installed.packages()[, 1] %in% "httr"))  install.packages("httr") 
     File.ASCII <- tempfile()
     on.exit(file.remove(File.ASCII))
     getTMP <- httr::GET(URL)
